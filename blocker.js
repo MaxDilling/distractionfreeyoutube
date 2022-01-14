fe4c32ed351c34a7718c4f6442c671bda1964a96
Max Dillinger <22136274+MaxDilling@users.noreply.github.com>
2022-01-14 22:33:36 +0100

    Update blocker.js

--- blocker.js
+++ blocker.js
@@ -2,6 +2,6 @@
 // @name        Distraction Free YouTube JS
 // @description This is your new file, start writing code
 // @match       *://www.youtube.com/*
-// @version     0.1
+// @version     0.1.1
 // @updateURL   https://raw.githubusercontent.com/MaxDilling/distractionfreeyoutube/main/blocker.js
 // ==/UserScript==
